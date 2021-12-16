c3c1a078a0846a1fb0e13b260fac286f224e55e9
tomerfatael <73110840+tomerfatael@users.noreply.github.com>
2021-12-16 18:42:45 +0200

    v

--- pfind.c
+++ pfind.c
@@ -1,9 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
 
+
+typedef struct DIRECTORY {
+    char path[PATH_MAX];
+    struct DIRECTORY *next;
+} DIRECTORY;
+
+typedef struct LIST {
+    DIRECTORY *head;
+    DIRECTORY *tail;
+} LIST;
+
+void enqueue(DIRECTORY *directory, DIRECTORY *head) {
+    
+}
 
 int main(int argc, char** argv) {
+    char *rootDirectory, *searchTerm;
+    int i,numOfThreads;
     if(argc != 4) {
         perror("invalid input");
         exit(1);
     }
+    rootDirectory = argv[1];
+    searchTerm = argv[2];
+    numOfThreads = argv[3];
+
 }
